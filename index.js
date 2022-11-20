06482063483ccc9f8337cc32b5fe3b72408dedb4
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2022-11-20 01:25:55 +0100

    1.2.4 - discord.js v14 update

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ const { Webhook } = require('dis-logs');
 const fs = require('fs');
 const auth = require('dotenv').config()
 const client = new Discord.Client({
-    intents: [ //This is new for me so i dont have clear what do they do
-        Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MEMBERS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES,
-        Discord.Intents.FLAGS.GUILD_WEBHOOKS,
-        Discord.Intents.FLAGS.DIRECT_MESSAGES
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.GuildWebhooks,
+        Discord.GatewayIntentBits.DirectMessages
     ],
     //partials: [
-    //    'CHANNEL', // Required to receive DMs
+    //    Discord.Partials.Channel, // Required to receive DMs
     //]
 });
 
 
 
 
-client.db = require('quick.db'); // Doing this we will enable quick.db globaly: client.db.get(`info`) 
+client.db = require('quick.db'); // Doing this, we will enable quick.db globaly: client.db.get(`info`) 
 client.ms = require('ms');
-client.config = require('./conf/config.json'); // Same with all these
+client.config = require('./conf/config.json'); // Same with all of these
 client.log = new Webhook(client.config.utils.log_webhook);
 
 client.discord = Discord;

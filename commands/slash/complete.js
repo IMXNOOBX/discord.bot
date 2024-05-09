fbe8552fe2a547f63c54659aefaa4b7aad29a70b
jonarango <paraelmobil403@gmail.com>
2024-05-09 11:38:56 +0200

    Update environment configuration
    - Add BOT_SERVERONLY flag to allow commands only in the server
    - Refactor interactionCreate.js to handle server-only commands
    - Refactor messageCreate.js to handle server-only commands
    - Update ready.js to display server-only flag in the ready log
    - Added auto complete example

--- commands/slash/complete.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'complete',
-    description: 'Show how to use many diferent options in a command',
-    run: async (client, interaction) => {
-        interaction.followUp({ content: `🚀 ping? *\`${client.ws.ping}\`*` })
-    }
-}
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,15 +1,18 @@
 const prefix = process.env.BOT_PREFIX || '!';
 
 module.exports.run = async (client, message) => {
-    if (message.author.bot || !message.guild) return;
+    if (message.author.bot) return;
+    if (process.env.BOT_SERVERONLY == 1 && !message.guild) return;
     if (!message.content.startsWith(prefix)) return;
     if (!message.member) message.guild.fetchMembers(message);
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g); // SCLICE to remove prefix, TRIM to remove spaces and SPLIT to make content in array
+    const args = message.content.slice(prefix.length).trim().split(/ +/g); // SCLICE to remove prefix, TRIM to remove spaces and SPLIT to make content an array
     const cmd = args.shift().toLowerCase(); // SHIFT commands to remove the prefix and toLowerCase to avoid capital letter like '!HeLp MoDeRaTiOn' 
     if (cmd.length === 0) return;
+
     let command = client.commands.normal.get(cmd);
-    if (!command) command = client.commands.normal.find(a => a.aliases && a.aliases.includes(cmd)); //Check aliases to use as commands
+    
+    if (!command) command = client.commands.normal.find(a => a.aliases && a.aliases.includes(cmd)); // Check aliases to use as commands
 
     if (!command) return; // if no command found (alias either) return
 

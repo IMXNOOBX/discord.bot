5dfd290c20805ad8ec162eb0bad88940490185cd
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2025-01-08 15:11:46 +0100

    Updated startup scripts, added dev & prod environment files, and removed unused packages

--- src/utilities/env.ts
+++ src/utilities/env.ts
@@ -1,9 +1,17 @@
 import log from './log';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: process.env.NODE_ENV === 'development' ? 
+        ['.env.development.local', '.env.local'] :
+        '.env.local'
+})
 
 const check_env = () => {
     const env = process.env;
+    const env_file = process.env.NODE_ENV === 'development' ? '.env.development.local, .env.local, .env' : '.env.local, .env'
 
-    log.info(`env - Running in ${env.NODE_ENV || 'production (default)'} mode`);
+    log.info(`env - Running in ${env.NODE_ENV || 'production (default)'} mode ${env_file}`);
 
     if (!env.BOT_TOKEN) 
         throw new Error('BOT_TOKEN not found in environment variables');
